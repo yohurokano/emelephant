5d8f378d9a8b29ab7b1ab5d56b782a7ec8bfb629
yohurokano <rvlcosta@gmail.com>
2025-04-13 03:14:23 +0100

    pictures

--- src/context/ClientThemeWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren, useContext } from "react";
-import { ThemeContext } from "./ThemeContext/ThemeContext";
-
-export default function ClientThemeWrapper({ children }: PropsWithChildren) {
-  const { theme } = useContext(ThemeContext);
-  return <div data-theme={theme}>{children}</div>;
-}
